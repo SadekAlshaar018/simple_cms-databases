15c5d85070d7354b8d7337a6b15fe8e3900170c0
SadekAlshaar018 <Your Actual Email, Sadek>
2018-02-13 17:33:36 +0100

    databases

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,7 +1,7 @@
 class Page < ApplicationRecord
 
     # belongs to subject optional true, and that would mean that we could save pages to our database
-  belongs_to :subject, { :optinal => false }
+  belongs_to :subject, { :optional => false }
 
-  # has_and_belongs_to_many :admin_users , :join_table => 'pages_admin_users'
+  has_and_belongs_to_many :admin_users
 end
