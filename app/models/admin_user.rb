c074c445ea422ac42180b270a16fef4e6dc0d56e
SadekAlshaar018 <Your Actual Email, Sadek>
2018-02-14 14:50:09 +0100

    Error
    stack level too deep

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,5 +1,8 @@
 class AdminUser < ApplicationRecord
   # self.table_name = "admin_user"
   has_and_belongs_to_many :pages
+
   has_many :section_edits
+  has_many :section_edits, :through => :section_edits
+  
 end
